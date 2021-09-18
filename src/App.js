507d1e02a85bebad28059bc0d8c95e6a467fe8a7
alicia639 <alicia.emberso.bremer@gmail.com>
2021-09-18 07:20:20 -0400

    adding two main blocks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import Blockly from 'blockly';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Algoraid (title?)</h1>
+        <xml id="toolbox" style="display: none">
+          <block type="controls_if"></block>
+          <block type="controls_repeat_ext"></block>
+          <block type="logic_compare"></block>
+          <block type="math_number"></block>
+          <block type="math_arithmetic"></block>
+          <block type="text"></block>
+          <block type="text_print"></block>
+        </xml>
+        <div class="blockly" id="blocklyDiv">
+        </div>
+        <div id="editor" class="edit">
+          text goes here
+        </div>
+        <script src="src/ace.js" type="text/javascript" charset="utf-8"></script>
+        <script>
+          var editor = ace.edit("editor");
+          var toolbox = '<xml>';
+          toolbox += '  <block type="controls_if"></block>';
+          toolbox += '  <block type="controls_whileUntil"></block>';
+          toolbox += '</xml>';
+          //var  workspace = Blockly.inject('blocklyDiv', {toolbox: toolbox});
+        </script>
       </header>
     </div>
   );
