411547215a409d4aa725a8d367f308c2561b08b9
alicia639 <alicia.emberso.bremer@gmail.com>
2021-09-18 07:42:03 -0400

    troubleshooting

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Algoraid (title?)</h1>
+        <div id="blocklyDiv" class="blockly">
+        </div>
+        <div id="editor" class="edit">
+          text goes here
+        </div>
+        <script src="src/ace.js" type="text/javascript" charset="utf-8"></script>
+        <script>
+          var editor = ace.edit("editor");
+        </script>
         <xml id="toolbox" style="display: none">
           <block type="controls_if"></block>
           <block type="controls_repeat_ext"></block>
@@ -16,19 +25,12 @@ function App() {
           <block type="text"></block>
           <block type="text_print"></block>
         </xml>
-        <div class="blockly" id="blocklyDiv">
-        </div>
-        <div id="editor" class="edit">
-          text goes here
-        </div>
-        <script src="src/ace.js" type="text/javascript" charset="utf-8"></script>
-        <script>
-          var editor = ace.edit("editor");
-          var toolbox = '<xml>';
-          toolbox += '  <block type="controls_if"></block>';
-          toolbox += '  <block type="controls_whileUntil"></block>';
-          toolbox += '</xml>';
-          //var  workspace = Blockly.inject('blocklyDiv', {toolbox: toolbox});
+        <script>  
+          var toolbox = document.getElementById(“toolbox”);
+
+          var options = {toolbox : toolbox};
+
+          Blockly.inject('blocklyDiv', options);
         </script>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,35 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-class Block extends React.component
-{
-  render() {
-
-    return (
-      <div>
-        <xml id="toolbox" style="display: none">
-          <block type="controls_if"></block>
-          <block type="controls_repeat_ext"></block>
-          <block type="logic_compare"></block>
-          <block type="math_number"></block>
-          <block type="math_arithmetic"></block>
-          <block type="text"></block>
-          <block type="text_print"></block>
-        </xml>
-        <div class="blockly" id="blocklyDiv">
-        </div>
-        <script>
-            var editor = ace.edit("editor");
-            var toolbox = '<xml>';
-            toolbox += '  <block type="controls_if"></block>';
-            toolbox += '  <block type="controls_whileUntil"></block>';
-            toolbox += '</xml>';
-            var workspace = Blockly.inject('blocklyDiv', {toolbox: toolbox});
-        </script>
-      </div>
-    );
-  }
-}
 
 ReactDOM.render(
   <React.StrictMode>
